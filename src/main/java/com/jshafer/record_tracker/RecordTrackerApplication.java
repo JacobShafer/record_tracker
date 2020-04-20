04840adf27f0f7ff34e4cf9b8b832131c39eb478
Jacob Shafer <jshafer55343@yahoo.com>
2020-04-19 19:08:37 -0500

    Base sprint boot application.

---
+++ src/main/java/com/jshafer/record_tracker/RecordTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jshafer.record_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jshafer/record_tracker/RecordTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshafer.record_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
